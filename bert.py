1a4a3fbe0c812ae3b00eac84984b39e31b2ecde9
jstephencorey <Yosef.d.kori@gmail.com>
2022-12-26 15:25:13 -0700

    added initial bert code

---
+++ bert.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+
+class BertModel(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    bert_model = BertModel()
